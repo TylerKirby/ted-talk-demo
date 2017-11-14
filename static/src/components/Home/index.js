3865c33436b01d1bac04aecc4c8c29148505da16
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-11-14 16:35:26 -0600

    updated frontend

--- static/src/components/Home/index.js
+++ static/src/components/Home/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const Home = () =>
     <section>
         <div className="container text-center">
-            <h1>Hello</h1>
+            <h1>Hello! Click the sidebar to register and begin searching transcripts</h1>
         </div>
     </section>;
